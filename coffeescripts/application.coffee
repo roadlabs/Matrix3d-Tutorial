587c676ad4b6fc097aac4d4166f76eb920805e81
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2010-09-16 01:28:07 +0200

    Added workaround for CSS inability to process scientific numbers

--- coffeescripts/application.coffee
+++ coffeescripts/application.coffee
@@ -50,10 +50,10 @@ initialize = ->
     
     # apply it via -webkit-transform
     s  = "matrix3d("
-    s += transformationMatrix.e(1,1) + "," + transformationMatrix.e(1,2) + "," + transformationMatrix.e(1,3) + "," + transformationMatrix.e(1,4) + ","
-    s += transformationMatrix.e(2,1) + "," + transformationMatrix.e(2,2) + "," + transformationMatrix.e(2,3) + "," + transformationMatrix.e(2,4) + ","
-    s += transformationMatrix.e(3,1) + "," + transformationMatrix.e(3,2) + "," + transformationMatrix.e(3,3) + "," + transformationMatrix.e(3,4) + ","
-    s += transformationMatrix.e(4,1) + "," + transformationMatrix.e(4,2) + "," + transformationMatrix.e(4,3) + "," + transformationMatrix.e(4,4)
+    s += transformationMatrix.e(1,1).toFixed(10) + "," + transformationMatrix.e(1,2).toFixed(10) + "," + transformationMatrix.e(1,3) + "," + transformationMatrix.e(1,4).toFixed(10) + ","
+    s += transformationMatrix.e(2,1).toFixed(10) + "," + transformationMatrix.e(2,2).toFixed(10) + "," + transformationMatrix.e(2,3) + "," + transformationMatrix.e(2,4).toFixed(10) + ","
+    s += transformationMatrix.e(3,1).toFixed(10) + "," + transformationMatrix.e(3,2).toFixed(10) + "," + transformationMatrix.e(3,3) + "," + transformationMatrix.e(3,4).toFixed(10) + ","
+    s += transformationMatrix.e(4,1).toFixed(10) + "," + transformationMatrix.e(4,2).toFixed(10) + "," + transformationMatrix.e(4,3) + "," + transformationMatrix.e(4,4).toFixed(10)
     s += ")"
     
     document.getElementById('darth-vader').style['-webkit-transform'] = s
--- javascripts/application.js
+++ javascripts/application.js
@@ -17,10 +17,10 @@
       translationMatrix = $M([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [Math.sin(rotateX * deg2rad) * 250 + 250, Math.sin(rotateY * deg2rad) * 150 + 150, 0, 1]]);
       transformationMatrix = rotationXMatrix.x(rotationYMatrix).x(rotationZMatrix).x(scaleMatrix).x(translationMatrix);
       s = "matrix3d(";
-      s += transformationMatrix.e(1, 1) + "," + transformationMatrix.e(1, 2) + "," + transformationMatrix.e(1, 3) + "," + transformationMatrix.e(1, 4) + ",";
-      s += transformationMatrix.e(2, 1) + "," + transformationMatrix.e(2, 2) + "," + transformationMatrix.e(2, 3) + "," + transformationMatrix.e(2, 4) + ",";
-      s += transformationMatrix.e(3, 1) + "," + transformationMatrix.e(3, 2) + "," + transformationMatrix.e(3, 3) + "," + transformationMatrix.e(3, 4) + ",";
-      s += transformationMatrix.e(4, 1) + "," + transformationMatrix.e(4, 2) + "," + transformationMatrix.e(4, 3) + "," + transformationMatrix.e(4, 4);
+      s += transformationMatrix.e(1, 1).toFixed(10) + "," + transformationMatrix.e(1, 2).toFixed(10) + "," + transformationMatrix.e(1, 3) + "," + transformationMatrix.e(1, 4).toFixed(10) + ",";
+      s += transformationMatrix.e(2, 1).toFixed(10) + "," + transformationMatrix.e(2, 2).toFixed(10) + "," + transformationMatrix.e(2, 3) + "," + transformationMatrix.e(2, 4).toFixed(10) + ",";
+      s += transformationMatrix.e(3, 1).toFixed(10) + "," + transformationMatrix.e(3, 2).toFixed(10) + "," + transformationMatrix.e(3, 3) + "," + transformationMatrix.e(3, 4).toFixed(10) + ",";
+      s += transformationMatrix.e(4, 1).toFixed(10) + "," + transformationMatrix.e(4, 2).toFixed(10) + "," + transformationMatrix.e(4, 3) + "," + transformationMatrix.e(4, 4).toFixed(10);
       s += ")";
       document.getElementById('darth-vader').style['-webkit-transform'] = s;
       rotateX -= 0.5;
